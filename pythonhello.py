b401426ecb748648a6092688cf022d3165c2bc1d
Ankita7520 <123752695+Ankita7520@users.noreply.github.com>
2023-05-24 18:35:50 +0530

    Add files via upload

--- pythonhello.py
+++ pythonhello.py
@@ -1,2 +1,7 @@
- print("hello world")
-hello world
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> var1=10
+>>> var2=20
+>>> sum=var1+var2
+>>> print("The sum of",var1,"and",var2,"is:",sum)
+The sum of 10 and 20 is: 30
